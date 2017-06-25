25d95c2bb0c3ee10007dcdbe2e5370734d24e55e
Omer <omer.g@proacts.com>
2017-06-25 17:05:22 +0300

    writing search by product name and price print values

--- WebSiteParsing.py
+++ WebSiteParsing.py
@@ -13,8 +13,18 @@ class Search:
          driver.get(self.url+name)
          html=driver.find_elements_by_class_name("product-item")
          products={}
+         i=1
          for product in html:
-             print product.find_element_by_class_name("name")
+             name= product.find_element_by_class_name("name").get_attribute("innerHTML")
+             price= product.find_element_by_css_selector("body > section > section > div > div > sp-items > div.items-wrapper > div.items > div:nth-child("+str(i)+") > div > sp-product > div.right.sp-product-price > span").get_attribute("innerHTML")
+             i=i+1
+             print name
+             print price
+
+         print i
+         driver.quit()
+
+
 
 
 
