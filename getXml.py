1300286ac5dee0dc90933a564a13c7592eea094b
Omer <omer.g@proacts.com>
2017-07-12 12:41:52 +0300

    download gz extract

--- getXml.py
+++ getXml.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import gzip
 
 driver = webdriver.WebDriver( "./chromedriver")
 
@@ -14,5 +15,13 @@ class getmarket(object):
         driver.find_element_by_id("ddlCategory").click()
         driver.find_element_by_css_selector("#ddlCategory > option:nth-child(3)").click()
         driver.find_element_by_css_selector("#gridContainer > table > tbody > tr:nth-child(1) > td:nth-child(1) > a").click()
-
+        sleep(30)
+        infile="/Users/omer/Downloads/"+ driver.find_element_by_css_selector("#gridContainer > table > tbody > tr:nth-child(1) > td:nth-child(7)").get_attribute("innerHTML")+".gz"
+        outfile="/Users/omer/Downloads/shop.xml"
         driver.quit()
+        inF = gzip.open(infile, 'rb')
+        outF = open(outfile, 'wb')
+        outF.write(inF.read())
+        inF.close()
+        outF.close()
+
